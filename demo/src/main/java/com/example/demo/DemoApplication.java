fbde501d90ffbf151e6a3b184360e3640b8a3b1c
Qixuan-lab <yuchengcai2006@gmail.com>
2026-02-07 20:00:59 +0800

    v3.0.0.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 		
 	public static void main(String[] args) {
-		System.out.println("成功运行");
+		System.out.println("成功运行v3.0.0");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
